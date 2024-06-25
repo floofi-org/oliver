26f642e714a91c50310cea416367886c805668b8
RaindropsSys <raindrops@equestria.dev>
2024-06-25 22:57:12 +0200

    Prepare multiple pages

--- build.js
+++ build.js
@@ -1,8 +1,8 @@
 #!/usr/bin/env node
-const release = require('./data/release.json');
+const release = require('./assets/data/release.json');
 
 release["timestamp"] = new Date().getTime();
 release["build"] = release['build'].substring(0, 3) + (parseInt(release.build.substring(3)) + 1);
 
-require('fs').writeFileSync("./data/release.json", JSON.stringify(release, null, 2));
+require('fs').writeFileSync("./assets/data/release.json", JSON.stringify(release, null, 2));
 require('child_process').execSync("vercel --prod", { stdio: "inherit" });
