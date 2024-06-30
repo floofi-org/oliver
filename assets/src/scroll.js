3d720508da5123658c5ce1554020fc1bdcdb1fe0
RaindropsSys <raindrops@equestria.dev>
2024-06-30 11:28:07 +0200

    Mobile improvements + WIP home page

--- assets/src/scroll.js
+++ assets/src/scroll.js
@@ -7,8 +7,10 @@ window.onscroll = () => {
 function updateScroll() {
     if (window.scrollY === 0) {
         document.getElementById("navbar").classList.add("no-border");
+        document.getElementById("mobile-navbar").classList.add("no-border");
     } else {
         document.getElementById("navbar").classList.remove("no-border");
+        document.getElementById("mobile-navbar").classList.remove("no-border");
     }
 
     for (let name of scrollHooks) {
