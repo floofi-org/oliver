fadcbfe73d3b192e22e7f273c2b6a2950a7cf747
Starscouts <starscouts@equestria.dev>
2024-08-13 15:08:56 +0200

    Add Polish and update credits

--- build.js
+++ build.js
@@ -5,8 +5,6 @@ release["timestamp"] = new Date().getTime();
 release["build"] = (parseInt(release['build'].split(".")[0]) + 1) + "." + release['build'].split(".").slice(1).join(",");
 
 require('fs').writeFileSync("./assets/data/release.json", JSON.stringify(release, null, 2));
-require('child_process').execSync("vercel --prod", { stdio: "inherit" });
-require('child_process').execSync("vercel --prod", { stdio: "inherit", cwd: "./shortener" });
 
 require('child_process').execSync("git add -A");
 require('child_process').execSync("git commit", { stdio: "inherit" });
@@ -14,3 +12,6 @@ require('child_process').execSync("git tag " + release["version"].split(" ")[0] 
 require('child_process').execSync("git push origin " + release["version"].split(" ")[0] + "." + release['build']);
 require('child_process').execSync("git push --all origin");
 require('child_process').execSync("gh release create " + release["version"].split(" ")[0] + "." + release['build'] + " --generate-notes");
+
+require('child_process').execSync("vercel --prod", { stdio: "inherit" });
+require('child_process').execSync("vercel --prod", { stdio: "inherit", cwd: "./shortener" });
